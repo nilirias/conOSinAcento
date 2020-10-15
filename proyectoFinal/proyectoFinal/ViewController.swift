7cd6045905b5267c290679d8720ad2335b5cef88
Nilirias <sepulvedac.nina@gmail.com>
2020-10-15 13:03:59 -0500

    Agrego de conexiones basicas entre pantallas

--- proyectoFinal/proyectoFinal/ViewController.swift
+++ proyectoFinal/proyectoFinal/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tfSeleccionDeRegla: UILabel!
+    @IBOutlet weak var bttnDerecha: UIButton!
+    @IBOutlet weak var bttnIzquierda: UIButton!
+    
+    var i = 0
+    
+    let listaDeReglas = ["Regla general", "Palabras en contexto", "Hiatos y diptongos"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func swipeDerecha(_ sender: UISwipeGestureRecognizer) {
+    }
+    
+    @IBAction func swipeIzquierda(_ sender: UISwipeGestureRecognizer) {
+    }
+    
+    
+    @IBAction func seleccionDerecha(_ sender: UIButton) {
+        if i == listaDeReglas.count-1 {
+            i = 0
+        } else {
+            i += 1
+        }
+        tfSeleccionDeRegla.text = listaDeReglas[i]
+    }
+    
+    @IBAction func seleccionIzquierda(_ sender: UIButton) {
+        if i == 0 {
+            i = listaDeReglas.count-1
+        } else {
+            i -= 1
+        }
+        tfSeleccionDeRegla.text = listaDeReglas[i]
+    }
 }
 
